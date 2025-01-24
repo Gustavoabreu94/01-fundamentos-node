a4c63f5eea33ff99ddac256eea49dbe51f490f8f
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-01-24 08:16:47 -0300

    create delete and update routes

--- src/server.js
+++ src/server.js
@@ -1,31 +1,24 @@
-import { randomUUID } from "node:crypto";
 import http from "node:http";
-import { Database } from "./database.js";
-import { jsson } from "./middlewares/json.js";
-
-const database = new Database();
+import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
-    return res.end(JSON.stringify(users));
+  await json(req, res);
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
+
+  if (route) {
+    const routeParams = req.url.match(route.path);
+
+    req.params = { ...routeParams.group };
+
+    return route.hander(req, res);
   }
 
-  await jsson(req, res);
-
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-    return res.writeHead(201).end();
-  }
   return res.writeHead(404).end("Not Found");
 });
 
